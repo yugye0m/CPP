6131734bc25dc9353ab3eecd2683486b0cd5071a
yugye0m <savior2002@naver.com>
2026-03-17 16:57:04 +0900

    CPP

---
+++ Week3/test2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+    for(int i = 5; i >=1; i--)
+    {
+
+        for(int j = 5; j >= 6-i; j--)
+        {
+            std::cout << j;
+        }
+
+        std::cout << "\n";
+    }
+}
